3395769137375b759aeff87f98e852c53e010358
KalongChan <82305211+KalongChan@users.noreply.github.com>
2023-03-13 19:09:54 +0800

    part of project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,155 @@
+import Link from "next/link";
+import Tilt from "react-parallax-tilt";
+import {BsGithub, BsBoxArrowUpRight} from "react-icons/bs";
+
 const Projects = () => {
   return (
-    <div className="projects" id="projects">
-      <div className="projects__main-title">Somethings I&#39;ve built</div>
+    <div className="project-group" id="projects">
+      <div className="project-group__main-title">Somethings I&#39;ve built</div>
       <div className="project">
-        <div className="project__img"></div>
+        {/* <img
+          src="/img/foodbearcat.png"
+          alt="foodbearcat"
+          className="project__img"
+        /> */}
+        <Tilt
+          tiltMaxAngleX={35}
+          tiltMaxAngleY={35}
+          perspective={900}
+          scale={1.1}
+          transitionSpeed={2000}
+          gyroscope={true}
+        >
+          <img className="project__img" src="/img/foodbearcat.png" />
+        </Tilt>
         <div className="project__detail">
-          <div className="project__detail-title"></div>
-          <div className="project__detail-desc"></div>
-          <div className="project__detail-stacks"></div>
-          <div className="project__detail-links"></div>
+          <div className="project__title">FoodBearCat</div>
+          <div className="project__desc">
+            A fullstack food ordering website with fully functional order,
+            register & login system
+          </div>
+          <div className="project__stacks">
+            <ul>
+              <li>React JS</li>
+              <li>Next.js</li>
+              <li>Redux</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+          <div className="project__links">
+            <Link
+              legacyBehavior
+              href="https://github.com/KalongChan/FoodBearCat"
+            >
+              <a target="_blank" className="project__detail-link">
+                <BsGithub />
+              </a>
+            </Link>
+            <Link legacyBehavior href="https://food-bear-cat.vercel.app/">
+              <a target="_blank" className="project__detail-link">
+                <BsBoxArrowUpRight />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* TEST */}
+
+      <div className="project">
+        {/* <img
+          src="/img/foodbearcat.png"
+          alt="foodbearcat"
+          className="project__img"
+        /> */}
+        <Tilt
+          tiltMaxAngleX={35}
+          tiltMaxAngleY={35}
+          perspective={900}
+          scale={1.1}
+          transitionSpeed={2000}
+          gyroscope={true}
+        >
+          <img className="project__img" src="/img/foodbearcat.png" />
+        </Tilt>
+        <div className="project__detail">
+          <div className="project__title">FoodBearCat</div>
+          <div className="project__desc">
+            A fullstack food ordering website with fully functional order,
+            register & login system
+          </div>
+          <div className="project__stacks">
+            <ul>
+              <li>React JS</li>
+              <li>Next.js</li>
+              <li>Redux</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+          <div className="project__links">
+            <Link
+              legacyBehavior
+              href="https://github.com/KalongChan/FoodBearCat"
+            >
+              <a target="_blank" className="project__detail-link">
+                <BsGithub />
+              </a>
+            </Link>
+            <Link legacyBehavior href="https://food-bear-cat.vercel.app/">
+              <a target="_blank" className="project__detail-link">
+                <BsBoxArrowUpRight />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* TEST */}
+      <div className="project">
+        {/* <img
+          src="/img/foodbearcat.png"
+          alt="foodbearcat"
+          className="project__img"
+        /> */}
+        <Tilt
+          tiltMaxAngleX={35}
+          tiltMaxAngleY={35}
+          perspective={900}
+          scale={1.1}
+          transitionSpeed={2000}
+          gyroscope={true}
+        >
+          <img className="project__img" src="/img/foodbearcat.png" />
+        </Tilt>
+        <div className="project__detail">
+          <div className="project__title">FoodBearCat</div>
+          <div className="project__desc">
+            A fullstack food ordering website with fully functional order,
+            register & login system
+          </div>
+          <div className="project__stacks">
+            <ul>
+              <li>React JS</li>
+              <li>Next.js</li>
+              <li>Redux</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+          <div className="project__links">
+            <Link
+              legacyBehavior
+              href="https://github.com/KalongChan/FoodBearCat"
+            >
+              <a target="_blank" className="project__link">
+                <BsGithub />
+              </a>
+            </Link>
+            <Link legacyBehavior href="https://food-bear-cat.vercel.app/">
+              <a target="_blank" className="project__link">
+                <BsBoxArrowUpRight />
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
