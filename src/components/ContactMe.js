f64d1b91aa30844b45456f0279086715ffecd47b
KalongChan <82305211+KalongChan@users.noreply.github.com>
2023-03-17 19:08:44 +0800

    ContactMe form styles && emailjs without api keys

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,9 +1,12 @@
-import Link from "next/link";
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import * as Yup from "yup";
 import {Formik, Form, Field} from "formik";
+import emailjs from "@emailjs/browser";
 
 const ContactMe = () => {
+  const [sendClicked, setSendClicked] = useState(false);
+  const [msgSent, setMsgSent] = useState(false);
+
   const formSchema = Yup.object().shape({
     email: Yup.string()
       .email("Invalid email")
@@ -12,24 +15,24 @@ const ContactMe = () => {
     message: Yup.string().required("Please enter your message"),
   });
 
-  const formItems = [
-    {type: "email", label: "Email"},
-    {type: "name", label: "Your Name"},
-    {type: "message", label: "Your Message"},
-  ];
-
   const initialValues = {email: "", name: "", message: ""};
 
-  const handleSubmit = (values) => {
-    // e.preventDefault();
-    console.log(values);
+  const handleSubmit = (values, resetForm) => {
+    setSendClicked(true);
+    setTimeout(async () => {
+      // mailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', values, 'YOUR_PUBLIC_KEY')
+
+      setMsgSent(true);
+      setSendClicked(false);
+      resetForm();
+    }, 1000);
   };
 
   return (
     <Fragment>
       <div className="contact__title">Contact Me</div>
       <div className="contact__text">
-        Feel free to contact me by clicking the button below
+        Feel free to contact me by completing the form below
       </div>
       <div className="contact__arrow">
         <img src="/img/up.png"></img>
@@ -37,32 +40,88 @@ const ContactMe = () => {
         <img src="/img/up.png"></img>
       </div>
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={formSchema}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
-        {({errors, touched}) => (
-          <Form>
-            <Field name="name" />
-            {errors.name && touched.name ? <div>{errors.name}</div> : null}
-            <Field name="email" />
-            {errors.email && touched.email ? <div>{errors.email}</div> : null}
-            <Field name="message" as="textarea" />
-            {errors.message && touched.message ? (
-              <div>{errors.message}</div>
-            ) : null}
-            <button className="contact__btn" type="submit">
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
+      <div className="contact__form">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={formSchema}
+          validateOnChange={false}
+          validateOnBlur={false}
+          onSubmit={(values, {resetForm}) => {
+            handleSubmit(values, resetForm);
+          }}
+        >
+          {({errors, touched}) => (
+            <Form>
+              <div className="contact__form-row">
+                <div className="contact__form-item">
+                  <div className="contact__form-error">
+                    {errors.name && touched.name ? errors.name : ""}
+                  </div>
+                  <Field
+                    name="name"
+                    id="name"
+                    className="contact__form-input"
+                    required
+                    form="novalidateform"
+                  />
+                  <label htmlFor="name" className="contact__form-label">
+                    Name
+                  </label>
+                </div>
 
-      <div className="contact__btn">
-        <Link href="mailto:kalongchan1113@gmail.com">Click Me</Link>
+                <div className="contact__form-item">
+                  <div className="contact__form-error">
+                    {errors.email && touched.email ? errors.email : ""}
+                  </div>
+
+                  <Field
+                    name="email"
+                    id="email"
+                    className="contact__form-input"
+                    required
+                    form="novalidateform"
+                  />
+                  <label htmlFor="email" className="contact__form-label">
+                    Email
+                  </label>
+                </div>
+              </div>
+
+              <div className="contact__form-row">
+                <div className="contact__form-item">
+                  <div className="contact__form-error">
+                    {errors.message && touched.message ? errors.message : ""}
+                  </div>
+
+                  <Field
+                    as="textarea"
+                    name="message"
+                    id="message"
+                    className="contact__form--message"
+                    required
+                    form="novalidateform"
+                  />
+                  <label
+                    htmlFor="message"
+                    className="contact__form-label--message"
+                  >
+                    Message
+                  </label>
+                  <div className="contact__form-submitted">
+                    {msgSent ? "Form submitted" : ""}
+                  </div>
+                </div>
+              </div>
+              <button
+                disabled={sendClicked}
+                className={`contact__form-btn`}
+                type="submit"
+              >
+                {sendClicked ? <span class="loader"></span> : "Submit"}
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </Fragment>
   );
